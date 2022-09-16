33f64f1d88cae95e5ee53d3d8db6330335db4934
IrisIq <2637852337@qq.com>
2022-09-16 08:42:49 +0800

    123

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,19 +6,33 @@ import type { MenuProps } from 'antd';
 import { HomeOutlined } from '@ant-design/icons';
 
 const items: MenuProps['items'] = [
-  { label: '菜单项一', key: 'item-1', icon: <HomeOutlined /> },
-  { label: '菜单项一', key: 'item-1', icon: <HomeOutlined /> },
-  { label: '菜单项一', key: 'item-1', icon: <HomeOutlined /> },
+  { label: '首页', key: '1', icon: <HomeOutlined /> },
+  { label: '分类', key: '2', icon: <HomeOutlined /> },
+  {
+    label: '工具',
+    key: '3',
+    icon: <HomeOutlined />,
+    children: [
+      {
+        label: '科学饮食计算器',
+        key: '3-1',
+      },
+      {
+        label: 'Option 1',
+        key: 'setting:1',
+      },
+    ],
+  },
 ];
 
 const Header = () => (
   <div className="header">
     <Row>
       <Col>
-        <span className="header_logo">Cookie_Duck</span>
+        <span className="header_logo">鸭鸭饼饼株式会社</span>
       </Col>
       <Col>
-        <span className="header_txt">鸭鸭饼饼的博客</span>
+        <span className="header_txt">这是他们的博客</span>
       </Col>
     </Row>
 
