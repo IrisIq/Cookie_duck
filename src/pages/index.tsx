a0c68145c21dfc207d2a9e4fbdc6a833c2e28631
IrisIq <2637852337@qq.com>
2022-09-17 10:03:16 +0800

    123

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
 import styles from './index.less';
-import { Button } from 'antd';
 import Header from '../components/Header';
+import { Col, Row, Menu } from 'antd';
+
+
+
 export default function IndexPage() {
   return (
     <div>
       <Header />
+
+      <Row className='comm-main' justify='center'>
+        <Col className='comm-left'>
+          左侧
+        </Col>
+        <Col className='comm-center'>
+          中间
+        </Col>
+        <Col className='comm-left'>
+          右侧
+        </Col>
+
+      </Row>
     </div>
   );
 }
