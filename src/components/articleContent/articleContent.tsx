dbd9a46a72b8529511baf688d5bdf7f911ba1420
IrisIq <2637852337@qq.com>
2023-10-07 17:55:22 +0800

    同步代码

--- src/components/articleContent/articleContent.tsx
+++ src/components/articleContent/articleContent.tsx
@@ -1,15 +1,24 @@
+import React, { ReactNode, useState, useEffect } from 'react';
 import { Card } from 'antd';
-import { history } from 'umi';
+import { useParams } from 'umi';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
-// interface ArticleContent {
-//   value: any;
-// }
+interface articleProps {
+  // value: any;
+  // route: any;
+}
 
-const ArticleContent: React.FC = (props) => {
+const ArticleContent: React.FC<articleProps> = (props) => {
   const markdown = `Just a link: https://reactjs.com.`;
+  useEffect(() => {
+    getArtical();
+  }, []);
 
+  const getArtical = () => {
+    // const value = useParams();
+    // console.log(value);
+  };
   return (
     <div className="artical-box">
       <Card hoverable>
--- src/pages/artical/index.tsx
+++ src/pages/artical/index.tsx
@@ -2,11 +2,14 @@ import { Fragment } from 'react';
 import IndexLayout from '@/components/layout/index';
 import BlogCard from '@/components/globles/BlogCard';
 import ArticleContent from '@/components/articleContent/articleContent';
+import { useParams } from 'umi';
+
 const Artical: React.FC = (props) => {
   const left = () => <BlogCard></BlogCard>;
   const top = () => <BlogCard> </BlogCard>;
 
   const center = () => <ArticleContent></ArticleContent>;
+  console.log(useParams());
 
   return (
     <Fragment>
